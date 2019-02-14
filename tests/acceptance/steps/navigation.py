20d25984375e23117e237af0eb848aec27210a86
unknown <shwethadjnaik@gmail.com>
2019-02-14 14:42:12 +0000

    Python selenium test cases for a web application

---
+++ tests/acceptance/steps/navigation.py
@@ -0,0 +1,47 @@
+from behave import *
+from selenium import webdriver
+import time
+
+from tests.acceptance.page_model.blog_page import BlogPage
+from tests.acceptance.page_model.home_page import HomePage
+from tests.acceptance.page_model.new_post_page import NewPostPage
+
+use_step_matcher('re')
+
+
+@given('I am on the homepage')
+def step_impl(context):
+    context.browser = webdriver.Chrome('C:\\Shwetha\\PythonTraining\\selenium-Section10\\code\\webdriver\\chromedriver.exe')
+    page = HomePage(context.browser)
+    context.browser.get(page.url)
+
+
+@given('I am on the blog page')
+def step_impl(context):
+    context.browser = webdriver.Chrome('C:\\Shwetha\\PythonTraining\\selenium-Section10\\code\\webdriver\\chromedriver.exe')
+    page = BlogPage(context.browser)
+    context.browser.get(page.url)
+
+
+@given('I am on the new post page')
+def step_impl(context):
+    context.browser = webdriver.Chrome(
+            'C:\\Shwetha\\PythonTraining\\selenium-Section10\\code\\webdriver\\chromedriver.exe')
+    page = NewPostPage(context.browser)
+    context.browser.get(page.url)
+
+
+@then('I am on the blog page')
+def step_impl(context):
+    expected_url = BlogPage(context.browser).url
+    assert context.browser.current_url == expected_url
+    time.sleep(2)
+
+
+@then('I am on the home page')
+def step_impl(context):
+    expected_url = HomePage(context.browser).url
+    print('Expected Url: ', expected_url)
+    print('Actual Url: ', context.browser.current_url)
+    assert context.browser.current_url == expected_url
+    time.sleep(2)
---
+++ tests/acceptance/steps/wait.py
@@ -0,0 +1,13 @@
+from behave import *
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+from tests.acceptance.locators.blog_page import BlogPageLocators
+
+use_step_matcher('re')
+
+@given('I wait for posts to load')
+def step_impl(context):
+    WebDriverWait(context.browser, 5).until(
+        expected_conditions.visibility_of_element_located(BlogPageLocators.POST_SECTION)
+    )
